3c9cce129bee45d166dec4849c0c65bc91a041b9
SinySs <dimakrytoi8@gmail.com>
2022-04-06 23:21:55 +0300

    Add some func

---
+++ Py/База/check_anagram.py
@@ -0,0 +1,3 @@
+def check_anagram(first_w, second_w):
+    return (sorted(first_w) == sorted(srcond_w))
+
---
+++ Py/База/input.py
@@ -0,0 +1,7 @@
+#Taking Two arg
+
+a, b = map(int, input().split())
+
+#Tahing list as input
+
+arr = list(map(int, input().split()))
